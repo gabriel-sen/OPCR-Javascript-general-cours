fa3356b82a4aa64831659979016daab61c55b2fd
gabriel <salut.gaby@gmail.com>
2018-02-09 21:32:18 +0100

    perimetre cercle reussit

--- chapitre_8-function/exercice/cercle/js/cercle.js
+++ chapitre_8-function/exercice/cercle/js/cercle.js
@@ -3,5 +3,12 @@ function aire(rayon){
   return aireCercle ;
 }
 
-
 console.log(aire(prompt("Entrez le rayon de votre cercle pour calculer l'aire")));
+
+
+function perimaitre(diametre){
+  perimetreCercle = diametre*Math.PI;
+  return perimetreCercle ;
+}
+
+console.log(aire(prompt("Entrez le diametre de votre cercle pour calculer son perimetre")));
