1b4404c2424e70ddac6022789f5d68b9531c0f4a
gabriel <salut.gaby@gmail.com>
2018-02-10 22:22:06 +0100

    chapitre_10-Programmation orienté objets dossiers créee

---
+++ chapitre_10 - programmatiOrienteObjets/cours/js/poo.js
@@ -0,0 +1,20 @@
+// pour obtenire la longueure d'une chaine de caractère sous forme d'une valeure Number on utilise la propriété .length
+console.log("ABC".length); // on obtiens 3
+
+// Ce qui est interessant c'estq ue cette valeur peut être stocké dans une variable.
+
+var mot = "Gabriel";
+var longueureMot = mot.length;
+console.log(longueureMot); // affiche 7
+
+// Il est possible de convertire une chaine de caractère en minuscule via la propriété .toLowerCase() ou en majuscule .toUpperCase
+
+var texte = "sen"
+var motEnMinusule = texte.toLowerCase();
+console.log(motEnMinusule);
+
+var motEnMajuscule = texte.toUpperCase();
+console.log(motEnMajuscule);
+
+
+// Les caractères sont numérotés de 0
