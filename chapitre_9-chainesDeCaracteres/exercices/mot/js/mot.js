e60b02902960aa5b702e2c2e69ca40b4c176ddb4
gabriel <salut.gaby@gmail.com>
2018-02-10 22:14:15 +0100

    Chapitre horrible La manipulation de caractères. 2 exo sur 4 Inutile pour moi sauf si un jours je décide de faire de la BDD. Sinon je ne ferais pas cette exo c'est extremement difficile.

--- chapitre_9-chainesDeCaracteres/exercices/mot/js/mot.js
+++ chapitre_9-chainesDeCaracteres/exercices/mot/js/mot.js
@@ -1,7 +1,26 @@
 var mot = "anaconda" ;
 var nombreCaracteres = mot.length;
-var motMajuscules = mot.toUpperCase()
-var motMinuscules = mot.toLowerCase()
+var motMajuscules = mot.toUpperCase();
+var motMinuscules = mot.toLowerCase();
+
+// Renvoie le nombre de voyelles d'un mot
+function compterNbVoyelles(mot) {
+    var nbVoyelles = 0;
+    for (var i = 0; i < mot.length; i++) {
+        var lettre = mot[i].toLowerCase();
+        if ((lettre === 'a') || (lettre === 'e') || (lettre === 'i') ||
+            (lettre === 'o') || (lettre === 'u') || (lettre === 'y')) {
+            nbVoyelles++;
+        }
+    }
+    return nbVoyelles;
+}
+
+
 console.log ("le mot "+ mot +" contiens "+ nombreCaracteres + " Caractères."  );
 console.log ("le mot "+ motMajuscules +" en majuscules"  );
 console.log ("le mot "+ motMinuscules +" en minuscules"  );
+
+
+var compterNbVoyelle = compterNbVoyelles ( mot );
+console.log ("le mot "+ mot +" contiens " + compterNbVoyelle + " voyelles." );
