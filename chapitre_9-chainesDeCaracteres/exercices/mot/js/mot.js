45b4d9cea13915f04c83bd181a394fa487f4556b
gabriel <salut.gaby@gmail.com>
2018-02-10 04:05:28 +0100

    début exercice chapitre 9... très chiant  et soporifique --'

---
+++ chapitre_9-chainesDeCaracteres/exercices/mot/js/mot.js
@@ -0,0 +1,7 @@
+var mot = "anaconda" ;
+var nombreCaracteres = mot.length;
+var motMajuscules = mot.toUpperCase()
+var motMinuscules = mot.toLowerCase()
+console.log ("le mot "+ mot +" contiens "+ nombreCaracteres + " Caractères."  );
+console.log ("le mot "+ motMajuscules +" en majuscules"  );
+console.log ("le mot "+ motMinuscules +" en minuscules"  );
