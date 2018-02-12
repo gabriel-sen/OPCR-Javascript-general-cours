97134309fa0cc1bcc01e5d8377eb5d65d4019534
gabriel <salut.gaby@gmail.com>
2018-02-12 23:19:20 +0100

    Exercice POO - compte en banque

---
+++ chapitre_10 - programmatiOrienteObjets/exercices/compte/js/compte.js
@@ -0,0 +1,26 @@
+var solde = 0;  // le compte est initialement de 0
+var entree = Number(prompt("Entrez une somme à créditer sur un compte à actuellement : " + solde + " €" )); // on récupère une valeure
+
+var compteAlexCrediter = { // Cette objet est le compte à créditer
+  titulaire:"Alex",
+  crediter: solde + entree, // cette propriété crédite le solde
+  crediterSolde : function () {
+    var descriptionCredit = "Titulaire "+ this.titulaire + " , solde : " + this.crediter + " euros.";
+    return  descriptionCredit;
+  }
+};
+
+
+var solde = compteAlexCrediter.crediterSolde; // on attribue le résultat de compte crédité à la valeure de solde qu'on s tock
+
+var entree = Number(prompt("Entrez une somme à débiter sur un compte à actuellement : " + solde + " €" )); // on entre une nouvelle donnée à stocker 
+
+var compteAlexDebiter = {
+  titulaire:"Alex",
+debiter: solde - entree, // cette propriété décrédite le solde
+debiterSolde : function () {
+  var descriptionCredit = "Titulaire "+ this.titulaire + " , solde : " + this.debiter + " euros."; // il reste sur le compte
+  return  descriptionCredit;
+}
+};
+console.log(compteAlexDebiter.debiterSolde()); // on affiche ce qu'il reste sur le compte.
