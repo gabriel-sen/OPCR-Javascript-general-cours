c08894d813c05aeae84402c794448b3081760a66
gabriel <salut.gaby@gmail.com>
2018-02-15 22:07:25 +0100

    Poo - cours la délégation

--- chapitre_11-POO-poussé/cours/js/poo_poussee.js
+++ chapitre_11-POO-poussé/cours/js/poo_poussee.js
@@ -5,7 +5,10 @@ var unObjet = {
   a:2
 };
 
-// Crée unAutreObjet avec unObjet comme prototypes
-var unAutreObjet = Object.create(unObjet);
-console.log(unAutreObjet.a);
-//Dans cet exemple, l'instruction JavaScriptObject.create() est utilisée pour créer l'objetunAutreObjet en lui donnant comme prototype l'objetunObjet. Lors de l'appel àunAutreObjet.a, c'est la propriétéa deunObjet qui est utilisée puisque la propriétéan'existe pas dansunAutreObjet.
+
+var unAutreObjet = Object.create(unObjet); // On créer un objet "unAutreObjet" avec comme propriété l'objet "unObjet"
+console.log(unAutreObjet.a); // on logg la propriété .a de "unObjet" a travers "unAutreObjet"
+// Si la propriété a n'esistais pas la valeur retourné aurait été "undefined"
+// On peux répéter ce chéma à l'infinie.
+var encoreUnAutreObjet = Object.create(unAutreObjet);
+console.log(encoreUnAutreObjet.a); //Ce mode de relation est appelé une Délégation.
