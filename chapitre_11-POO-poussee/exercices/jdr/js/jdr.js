605370f28d00c084fdd2303354300fc202b4064c
gabriel <salut.gaby@gmail.com>
2018-02-19 00:48:24 +0100

    Exo JDR amélioré fini ajout de pieces d'or et de clef dans le jeux.

--- chapitre_11-POO-poussee/exercices/jdr/js/jdr.js
+++ chapitre_11-POO-poussee/exercices/jdr/js/jdr.js
@@ -4,8 +4,8 @@ var Personnage = {
         this.nom = nom;
         this.sante = sante;
         this.force = force;
-        this.pieceOr = pieceOr;
-        this.clef = clef;
+        this.pieceOr = 10;
+        this.clef = 1;
     },
     // Attaque un personnage cible
     attaquer: function (cible) {
@@ -14,10 +14,13 @@ var Personnage = {
             console.log(this.nom + " attaque " + cible.nom + " et lui fait " + degats + " points de dégâts");
             cible.sante = cible.sante - degats;
             if (cible.sante > 0) {
-                console.log(cible.nom + " a encore " + cible.sante + " points de vie");
+                console.log(cible.nom + " a encore " + cible.sante + " points de vie, " + this.pieceOr + " pieces d'or et " + this.clef + " clés");
             } else {
                 cible.sante = 0;
                 console.log(cible.nom + " est mort !");
+                console.log( joueur1.nom + " et " + joueur2.nom + " récupérent chacuns : " + cible.pieceOr + " pieces d'or sur  "+ " et " + cible.clef + " clef sur  "+ cible.nom );
+                this.pieceOr = cible.pieceOr + this.pieceOr;
+                cible.pieceOr = 0
             }
         } else {
             console.log(this.nom + " ne peut pas attaquer : il est mort...");
@@ -34,15 +37,14 @@ Joueur.initJoueur = function (nom, sante, force) {
 // Renvoie la description du joueur
 Joueur.decrire = function () {
     var description = this.nom + " a " + this.sante + " points de vie, " +
-        this.force + " en force et " + this.xp + " points d'expérience";
+        this.force + " en force et " + this.xp + " points d'expérience " + this.pieceOr + " pieces d'or " + this.clef + " clés" ;
     return description;
 };
 // Combat un adversaire
 Joueur.combattre = function (adversaire) {
     this.attaquer(adversaire);
     if (adversaire.sante === 0) {
-        console.log(this.nom + " a tué " + adversaire.nom + " et gagne " +
-            adversaire.valeur + " points d'expérience");
+        console.log(this.nom + " a tué " + adversaire.nom + " et gagne " +  adversaire.valeur + " points d'expérience");
         this.xp += adversaire.valeur;
     }
 };
@@ -53,8 +55,8 @@ Adversaire.initAdversaire = function (nom, sante, force, race, valeur, pieceOr, 
     this.initPerso(nom, sante, force);
     this.race = race;
     this.valeur = valeur;
-    this.pieceOr = pieceOr;
-    this.clef = clef;
+    this.pieceOr = 10;
+    this.clef = 1;
 };
 
 // ...
@@ -62,17 +64,17 @@ Adversaire.initAdversaire = function (nom, sante, force, race, valeur, pieceOr, 
 // ...
 
 var joueur1 = Object.create(Joueur);
-joueur1.initJoueur("Aurora", 150, 25, 10, 1);
+joueur1.initJoueur("Aurora", 150, 25);
 
 var joueur2 = Object.create(Joueur);
-joueur2.initJoueur("Glacius", 130, 30, 10, 1);
+joueur2.initJoueur("Glacius", 130, 30);
 
 console.log("Bienvenue dans ce jeu d'aventure ! Voici nos courageux héros :");
 console.log(joueur1.decrire());
 console.log(joueur2.decrire());
 
 var monstre = Object.create(Adversaire);
-monstre.initAdversaire("ZogZog", 40, 20, "orc", 10, 10, 1);
+monstre.initAdversaire("ZogZog", 40, 20, "orc", 10);
 
 console.log("Un affreux monstre arrive : c'est un " + monstre.race + " nommé " + monstre.nom);
 
