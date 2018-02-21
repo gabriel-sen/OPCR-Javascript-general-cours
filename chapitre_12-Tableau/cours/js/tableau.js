f789698ed48fff24bf85904b2d151c60ea3c7915
gabriel <salut.gaby@gmail.com>
2018-02-21 23:21:00 +0100

    Avané sur le cours des tableau.

--- chapitre_12-Tableau/cours/js/tableau.js
+++ chapitre_12-Tableau/cours/js/tableau.js
@@ -2,3 +2,45 @@ var films = ["Le loup de Wall Street", "Vice-Versa", "Babystitting"];
 // Retourner le nombe de films
 
 console.log(films.length);
+
+// Les tableaux peuvent stocker plusieurs informations de differents types :
+var tab = [ "Bonjour", 7, true]
+
+//on peux savoir le nombre d'élements dans le tableau via la propriété .length
+
+var films = ["le loup de Wallstreet", "vice-versa", "Babystitting"];
+console.log(films.length)
+
+// Un tableau vide affichera 0 exemple :
+var filmeVide = [];
+console.log(filmeVide.length);
+
+// Le décompte commence pas à 1 mais à 0
+var mesFilms = ["Deadpool", "Pokemon", "Tencendance"];
+console.log(mesFilms[0]); // Affiche Deadpool
+console.log(mesFilms[1]); // Affiche Pokemon
+console.log(mesFilms[2]); // Affiche Tendance
+
+
+// RETOURNER LES ELENTS :
+// Pour automatiser l'affichage des films
+
+var filmsLoop = ["Deadpool 2", "Pokemon 2", "Tencendance 2"];
+for (var i = 0; i < filmsLoop.length; i++) {
+    console.log(filmsLoop[i]);
+}
+
+// La methode forEach() permet d'appliquer une fonction sur chaque éléments du tableau.
+
+var filmElements = ["le loup de Wallstreet", "vice-versa", "Babystitting"];
+filmElements.forEach(function(film){
+    console.log(film);
+});
+
+// Ajouter des elements au tableau via  la methode .push
+var filmsPush = ["Deadpool 2", "Pokemon 2", "Tencendance 2"];
+filmsPush.push(" Les bronzés")
+console.log(filmsPush[3])
+
+
+// 
